6eadbb130e84d518dc27c364ce11b9935f6c1929
LichengCAO <licheng_cao@126.com>
2025-12-23 23:27:04 +0800

    wrap enki lib

--- src/utility/task_scheduler.cpp
+++ src/utility/task_scheduler.cpp
@@ -1,1 +1,98 @@
 #include "task_scheduler.h"
+#include <TaskScheduler.h>
+
+MyTaskScheduler::MyTaskScheduler()
+	:m_threadCount(4)
+{
+	m_uptrTaskSchedular = std::make_unique<enki::TaskScheduler>();
+	m_uptrTaskSchedular->Initialize(m_threadCount);
+}
+
+MyTaskScheduler& MyTaskScheduler::GetInstance()
+{
+	if (g_uptrInstance == nullptr)
+	{
+		g_uptrInstance = std::make_unique<MyTaskScheduler>();
+	}
+	return *g_uptrInstance;
+}
+
+MyTaskScheduler::~MyTaskScheduler()
+{
+}
+
+void MyTaskScheduler::AddSingleThreadTask(MySingleThreadTask* pSingleThreadTask, uint32_t inThreadIndex)
+{
+	pSingleThreadTask->AssignToSchedular(this, inThreadIndex);
+}
+
+void MyTaskScheduler::AddMutiThreadTask(MyMultiThreadTask* pMultiThreadTask, uint32_t inSubTaskCount)
+{
+	pMultiThreadTask->AssignToSchedular(this, inSubTaskCount);
+}
+
+void MyTaskScheduler::WaitForTask(const IWaitable* pToWait)
+{
+	pToWait->WaitForThis(this);
+}
+
+void MyTaskScheduler::WaitForAll()
+{
+	m_uptrTaskSchedular->WaitforAll();
+}
+
+void MySingleThreadTask::WaitForThis(MyTaskScheduler* pSchedular) const
+{
+	pSchedular->m_uptrTaskSchedular->WaitforTask(m_uptrTask.get());
+}
+
+void MySingleThreadTask::AssignToSchedular(MyTaskScheduler* pSchedular, uint32_t inThreadId)
+{
+	if (m_uptrTask == nullptr)
+	{
+		m_uptrTask = std::make_unique<enki::LambdaPinnedTask>(inThreadId, [&]()
+			{
+				this->Execute();
+			}
+		);
+	}
+	pSchedular->m_uptrTaskSchedular->AddPinnedTask(m_uptrTask.get());
+}
+
+void MyMultiThreadTask::WaitForThis(MyTaskScheduler* pSchedular) const
+{
+	pSchedular->m_uptrTaskSchedular->WaitforTask(m_uptrTask.get());
+}
+
+void MyMultiThreadTask::AssignToSchedular(MyTaskScheduler* pSchedular, uint32_t inSubTaskCount)
+{
+	if (m_uptrTask == nullptr)
+	{
+		m_uptrTask = std::make_unique<enki::TaskSet>(inSubTaskCount, [&](enki::TaskSetPartition inRange, uint32_t inThreadId)
+			{
+				this->ExecuteSubTask(inRange.start, inRange.end, inThreadId);
+			}
+		);
+	}
+	pSchedular->m_uptrTaskSchedular->AddTaskSetToPipe(m_uptrTask.get());
+}
+
+MySingleThreadTaskLambda::MySingleThreadTaskLambda(std::function<void()> lambda)
+	:m_function(lambda)
+{
+}
+
+void MySingleThreadTaskLambda::Execute()
+{
+	m_function();
+}
+
+MyMultiThreadTaskLambda::MyMultiThreadTaskLambda(std::function<void(uint32_t, uint32_t, uint32_t)> lambda)
+	:m_function(lambda)
+{
+}
+
+void MyMultiThreadTaskLambda::ExecuteSubTask(uint32_t SubStart, uint32_t SubEnd, uint32_t inThreadIndex)
+{
+	m_function(SubStart, SubEnd, inThreadIndex);
+}
--- src/utility/task_scheduler.h
+++ src/utility/task_scheduler.h
@@ -1,42 +1,93 @@
 #pragma once
-#include <enkiTS/src/TaskScheduler.h>
+#include "common.h"
+#include <functional>
 
+namespace enki
+{
+	class LambdaPinnedTask;
+	class TaskSet;
+	class TaskScheduler;
+}
 class MyTaskScheduler;
 
 class IWaitable
 {
 public:
-	virtual void WaitForThis(MyTaskScheduler*) = 0;
+	virtual void WaitForThis(MyTaskScheduler*) const = 0;
 };
 
-class ISingleThreadTask : public IWaitable
+class MySingleThreadTask : public IWaitable
 {
+private:
+	std::unique_ptr<enki::LambdaPinnedTask> m_uptrTask;
+
 public:
-	virtual void AssignToThread(MyTaskScheduler*) = 0;
+	virtual void WaitForThis(MyTaskScheduler* pSchedular) const override;
+	
+	void AssignToSchedular(MyTaskScheduler* pSchedular, uint32_t inThreadId = 0);
+
 	virtual void Execute() = 0;
 };
 
-class IMultiThreadTask : public IWaitable
-{
-public:
-	virtual void AssignToThreadGroup(MyTaskScheduler*) = 0;
-	virtual void Execute(uint32_t inThreadIndex) = 0;
-};
-
-class MyTaskScheduler final
+class MyMultiThreadTask : public IWaitable
 {
 private:
-	static std::unique_ptr<MyTaskScheduler> m_uptrInstance;
+	std::unique_ptr<enki::TaskSet> m_uptrTask;
 
+public:
+	virtual void WaitForThis(MyTaskScheduler* pSchedular) const override;
+
+	void AssignToSchedular(MyTaskScheduler* pSchedular, uint32_t inSubTaskCount = 1);
+
+	virtual void ExecuteSubTask(uint32_t SubStart, uint32_t SubEnd, uint32_t inThreadIndex) = 0;
+};
+
+class MySingleThreadTaskLambda : public MySingleThreadTask
+{
+private:
+	std::function<void()> m_function;
+
+public:
+	MySingleThreadTaskLambda(std::function<void()> lambda);
+
+	virtual void Execute() override;
+};
+
+class MyMultiThreadTaskLambda : public MyMultiThreadTask
+{
+private:
+	std::function<void(uint32_t, uint32_t, uint32_t)> m_function;
+
+public:
+	MyMultiThreadTaskLambda(std::function<void(uint32_t, uint32_t, uint32_t)> lambda);
+
+	virtual void ExecuteSubTask(uint32_t SubStart, uint32_t SubEnd, uint32_t inThreadIndex) override;
+};
+
+class MyTaskScheduler
+{
+private:
+	static std::unique_ptr<MyTaskScheduler> g_uptrInstance;
 	MyTaskScheduler();
+
+public:
+	static MyTaskScheduler& GetInstance();
+
+private:
+	std::unique_ptr <enki::TaskScheduler> m_uptrTaskSchedular;
+	uint32_t m_threadCount;
 
 public:
 	~MyTaskScheduler();
 
-	static MyTaskScheduler& GetInstance();
+	void AddSingleThreadTask(MySingleThreadTask* pSingleThreadTask, uint32_t inThreadIndex = 0);
+
+	void AddMutiThreadTask(MyMultiThreadTask* pMultiThreadTask, uint32_t inSubTaskCount = 1);
 
 	void WaitForTask(const IWaitable* pToWait);
 
-
 	void WaitForAll();
+
+	friend class MySingleThreadTask;
+	friend class MyMultiThreadTask;
 };
