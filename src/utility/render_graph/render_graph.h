f61999f8a0aab678062ae636e67a7ccd830b448e
LichengCAO <licheng_cao@126.com>
2026-08-10 21:51:17 +0800

    update render graph

--- src/utility/render_graph/render_graph.h
+++ src/utility/render_graph/render_graph.h
@@ -6,6 +6,15 @@ using RenderGraphImageHandle = uint32_t;
 using RenderGraphPassHandle = uint32_t;
 class Buffer;
 class Image;
+class CommandBuffer;
+class Command;
+
+enum class RenderGraphPassType
+{
+	COMPUTE,  // Owns compute queue commands
+	GRAPHICS, // Owns graphics queue commands, manages render pass itself  
+	SUBPASS,  // Owns graphics queue commands in a subpass, let graph manage render pass for it
+};
 
 class RenderGraph
 {
@@ -23,7 +32,7 @@ public:
 	public:
 		void SetFormat(VkFormat inFormat);
 		void CustomizeMipLevel(uint32_t inMipLevelCount);
-		void CustomizArrayLayer(uint32_t inLayerCount);
+		void CustomizeArrayLayer(uint32_t inLayerCount);
 		void CustomizeSize(uint32_t inWidth, std::optional<uint32_t> inHeight = {}, std::optional<uint32_t> inDepth = {});
 		void AddUsages(VkImageUsageFlags inUsage);
 		void SetAsExternal();
@@ -32,28 +41,47 @@ public:
 	class PassInfo
 	{
 	public:
+		virtual ~PassInfo() = default;
+		virtual auto GetType() const-> RenderGraphPassType = 0;
+		void AddSampledImage(RenderGraphImageHandle inHandle, VkPipelineStageFlags2 inReadStage);
+		void AddStorageImage(RenderGraphImageHandle inHandle, VkPipelineStageFlags2 inWriteStage);
+		void AddDescriptorUniformBuffer(RenderGraphBufferHandle inHandle, VkPipelineStageFlags2 inReadStage);
+		void AddDescriptorStorageBuffer(RenderGraphBufferHandle inHandle, VkPipelineStageFlags2 inWriteStage);
+	};
+
+	class ComputePassInfo final : public PassInfo
+	{
+	public:
+		explicit ComputePassInfo() = default;
+		virtual auto GetType() const-> RenderGraphPassType override { return RenderGraphPassType::COMPUTE; }
+	};
+
+	class GraphicsPassInfo final : public PassInfo
+	{
+	public:
+		explicit GraphicsPassInfo() = default;
+		virtual auto GetType() const-> RenderGraphPassType override { return RenderGraphPassType::GRAPHICS; }
+	};
+
+	class SubpassInfo final : public PassInfo
+	{
+	public:
+		explicit SubpassInfo() = default;
+		virtual auto GetType() const-> RenderGraphPassType override { return RenderGraphPassType::SUBPASS; }
+		void UseDedicateRenderPass();
 		void AddColorAttachment(
-			RenderGraphImageHandle inHandle, 
+			RenderGraphImageHandle inHandle,
 			VkAttachmentLoadOp inLoadOp,
-			VkImageLayout inLoadLayout, 
 			VkPipelineStageFlags2 inLoadStage,
 			VkAttachmentStoreOp inStoreOp,
-			VkImageLayout inStoreLayout, 
 			VkPipelineStageFlags2 inStoreStage);
 		void AddDepthAttachment(
-			RenderGraphImageHandle inHandle, 
+			RenderGraphImageHandle inHandle,
 			VkAttachmentLoadOp inLoadOp,
-			VkImageLayout inLoadLayout,
 			VkPipelineStageFlags2 inLoadStage,
 			VkAttachmentStoreOp inStoreOp,
-			VkImageLayout inStoreLayout,
 			VkPipelineStageFlags2 inStoreStage);
 		void AddDescriptorInputAttachment(RenderGraphImageHandle inHandle, VkPipelineStageFlags2 inReadStage);
-		void AddDescriptorReadOnlyImage(RenderGraphImageHandle inHandle, VkPipelineStageFlags2 inReadStage);
-		void AddDescriptorWriteOnlyImage(RenderGraphImageHandle inHandle, VkPipelineStageFlags2 inWriteStage);
-		void AddDescriptorReadWriteImage(RenderGraphImageHandle inHandle, VkPipelineStageFlags2 inReadStage, VkPipelineStageFlags2 inWriteStage);
-		void AddDescriptorUniformBuffer(RenderGraphBufferHandle inHandle, VkPipelineStageFlags2 inReadStage);
-		void AddDescriptorStorageBuffer(RenderGraphBufferHandle inHandle, VkPipelineStageFlags2 inReadStage, VkPipelineStageFlags2 inWriteStage);
 	};
 
 public:
@@ -62,6 +90,10 @@ public:
 	auto RegisterImage(const std::string& inName, const RenderGraph::ImageInfo& inImageInfo) -> RenderGraphImageHandle;
 	auto GetImageHandle(const std::string& inName)const -> RenderGraphImageHandle;
 	auto AddPass(const std::string& inName, const RenderGraph::PassInfo& inPassInfo)-> RenderGraphPassHandle;
+	void AddExtraDependency(RenderGraphPassHandle inHappensSooner, RenderGraphPassHandle inHappensLater);
+	void Build();
+	//TODO: Maybe it's unneccessary.
+	//void SetAliasingRule(std::function<bool(RenderGraphImageHandle inOrigin, RenderGraphImageHandle inAliasCandidate)> inImageRule, std::function<bool(RenderGraphBufferHandle inOrigin, RenderGraphBufferHandle inAliasCandidate)> inBufferRule);
 };
 
 class RenderGraphInstance
@@ -69,7 +101,7 @@ class RenderGraphInstance
 public:
 	struct ExternalBufferInfo
 	{
-		Buffer* pBuffer;
+		Buffer* pBuffer{};
 		VkPipelineStageFlags2 enteringStage;
 		VkAccessFlags2 enteringAccess;
 		VkPipelineStageFlags2 leavingStage;
@@ -78,7 +110,7 @@ public:
 
 	struct ExternalImageInfo
 	{
-		Image* pImage;
+		Image* pImage{};
 		VkImageLayout enteringLayout; 
 		VkPipelineStageFlags2 enteringStage; 
 		VkAccessFlags2 enteringAccess;
@@ -89,7 +121,11 @@ public:
 
 	class ExecutionContext
 	{
-
+	public:
+		auto ResolveBuffer(RenderGraphBufferHandle inHandle) -> Buffer*;
+		auto ResolveImage(RenderGraphImageHandle inHandle) -> Image*;
+		void FillSubpassCommands(RenderGraphPassHandle inTarget, std::vector<const Command*> inCommands);
+		void RecordCommandBuffer(RenderGraphPassHandle inTarget, std::function<void(CommandBuffer*)> inProcess);
 	};
 
 	class PassInfo
@@ -98,11 +134,14 @@ public:
 		void SetProcess(std::function<void(RenderGraphInstance::ExecutionContext&)> inProcess);
 	};
 
+private:
+	void _SetUpPhysicalResources();
+
 public:
 	RenderGraphInstance(const RenderGraph& inRenderGraph);
 	void SetUpExternalBuffer(RenderGraphBufferHandle inHandle, const RenderGraphInstance::ExternalBufferInfo& inBufferInfo);
 	void SetUpExternalImage(RenderGraphImageHandle inHandle, const RenderGraphInstance::ExternalImageInfo& inImageInfo);
 	void SetUpPass(RenderGraphPassHandle inHandle, const RenderGraphInstance::PassInfo& inPassInfo);
-	auto ResolveBuffer(RenderGraphBufferHandle inHandle) -> Buffer*;
-	auto ResolveImage(RenderGraphImageHandle inHandle) -> Image*;
+	void Compile();
+	void Execute();
 };
